5d1a4a4938588b456d24e7d7a748c377f3fcf23d
Rodrigo Jose Fagundes <rodrigofgds@hotmail.com>
2023-03-04 17:16:50 -0300

    Expresssao condicional ternaria
    Aula 40.

---
+++ 40 - Expressao condicional ternaria/1/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+public static void main(String [] args) {
+Scanner sc = new Scanner (System.in);
+
+double preco = 34.5;
+double desconto;
+
+if (preco < 20.0) {
+
+desconto = preco * 0.1;
+
+}
+else {
+desconto = preco * 0.05;
+}
+
+System.out.println(desconto);
+
+sc.close();
+
+
+}
+}
