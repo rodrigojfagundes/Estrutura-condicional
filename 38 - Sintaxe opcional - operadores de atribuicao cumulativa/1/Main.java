7c174de23e067247eabc3f9eae722aca5654d033
Rodrigo Jose Fagundes <rodrigofgds@hotmail.com>
2023-03-04 17:13:17 -0300

    Sintaxe opcional - operadores de atribuicao
    Aula 38.

---
+++ 38 - Sintaxe opcional - operadores de atribuicao cumulativa/1/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+public static void main(String [] args) {
+
+Scanner sc = new Scanner(System.in);
+
+int minutos = sc.nextInt();
+
+double conta = 50.0;
+
+if (minutos > 100) {
+conta = conta + (minutos - 100) * 2.0;
+
+}
+
+System.out.printf("o valor da conta e = R$ %.2f%n", conta);
+
+sc.close();
+
+
+}
+}
