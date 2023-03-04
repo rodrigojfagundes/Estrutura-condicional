03cbf9b04a6719eb391bf07cfb3dbb63acaf6385
Rodrigo Jose Fagundes <rodrigofgds@hotmail.com>
2023-03-04 17:09:03 -0300

    Estrutura condicional (if-else)
    Aula 35.

---
+++ 35 - Estrutura condicional (if-else)/1/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+public static void main(String [] args) {
+
+
+int x = 5;
+
+
+System.out.println("bom dia");
+
+if(x < 0) {
+
+System.out.println("bom tarde");
+
+
+}
+
+
+System.out.println("boa noite");
+
+
+}
+}
